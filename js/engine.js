ee7422f45cac9c619a7f8f4555867f81376515a6
a-zz <antonio@pareidolia.es>
2017-03-06 11:08:06 +0100

    Function description added to engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,12 +1,19 @@
 /* ************************************************************************** 
  * Mini JavaScript-Only CMS - Engine
  * ************************************************************************** */
+
+// This function should be fired on body.onload()!
 function start()
 {
 	drawHeader();
 	navigate();
 }
 
+// Renders the site header (logo and menu so far).
+// Arguments: 
+//	(none)
+// Returns:
+//	(nothing)
 function drawHeader()
 {
 	// Draw the logo (site name & description)
@@ -21,6 +28,11 @@ function drawHeader()
 	hdrmenu.innerHTML = innerHTML;
 }
 
+// Navigate to some content (referred in the URL)
+// Arguments: 
+//	(none)
+// Returns:
+//	(nothing) 
 function navigate()
 {	
 	var url = window.location.href;
@@ -46,6 +58,12 @@ function navigate()
 		loadPage(pageId);
 }
 
+// Load a content page
+// Arguments:
+//	pageId (String) The page to load, referred by its id (field pageid in 
+//		metadata.json > site > pages).
+// Returns:
+//	(nothing)
 function loadPage(pageId)
 {
 	var cntnr = window.document.getElementById('main');
@@ -61,6 +79,11 @@ function loadPage(pageId)
 	}
 }
 
+// Find the last content page
+// Arguments:
+//	(none)
+// Returns:
+//	(String) The last page id (field pageid in metadata.json > site > pages).
 function findLastPage()
 {
 	var lastDate = 0;
@@ -77,6 +100,11 @@ function findLastPage()
 	return lastPageId;
 }
 
+// Find a random page
+// Arguments:
+// 	(none)
+// Returns:
+// 	(String) The last page id (field pageid in metadata.json > site > pages).
 function randomPage()
 {
 	return site.pages[Math.floor(Math.random() * site.pages.length)].pageid;
